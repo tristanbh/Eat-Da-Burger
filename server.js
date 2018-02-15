e238db25104592d1e82279e81b502daa4aa04e46
Tristan Benedict-Hall <tbenedicthall@Tristans-MacBook-Pro.local>
2018-02-15 14:23:42 -0500

    changed server.js port listen function, currently troubleshooting app crash on heroku

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var app = express();
 
 var port = process.env.PORT || 3000;
-
-var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -21,4 +20,6 @@ var routes = require("./controllers/burgersController.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(port, function(){
+    
+});
