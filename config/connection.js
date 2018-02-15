b63491dfb068b6efc39143c210cd7107195f1cc5
Tristan Benedict-Hall <tbenedicthall@Tristans-MacBook-Pro.local>
2018-02-15 13:09:25 -0500

    changed some settings in connection.js, installed dotenv

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ if(process.env.JAWSDB_URL){
 }else{
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-  port: 3306,
+  port: 3000,
   host: "localhost",
   user: "root",
   password: "",
